be959e90f40b03777f51e8e6a4df6db27e7dbf07
Douglas Badaró <douglas.badaro@outlook.com>
2023-07-26 18:33:42 -0300

    create project

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Trabalho PHP</title>
+</head>
+
+<body style="display: flex; flex-direction: column;">
+  <h1>Trabalho PHP</h1>
+  <a href="questions/question1.php">Questão 1</a>
+  <a href="questions/question2.php">Questão 2</a>
+  <a href="questions/question3.php">Questão 3</a>
+  <a href="questions/question4.php">Questão 4</a>
+</body>
+
+</html>
